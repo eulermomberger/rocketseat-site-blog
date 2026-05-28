537ac20d57a5a475e36986a5d776ef42f9e98126
euler <mombergereuler@gmail.com>
2026-05-27 22:27:21 -0300

    ♻️ refactor: Aplicando SSG no blog

--- src/components/templates/blog/index.tsx
+++ src/components/templates/blog/index.tsx
@@ -1,22 +1,27 @@
 import { Search } from '@/components/Search';
 import { useRouter } from 'next/router';
 import { Inbox } from 'lucide-react';
+import { Post } from 'contentlayer/generated';
+
 import { PostCard } from './components/PostCard';
 import { PostGridCard } from './components/PostGridCard';
-import { allPosts } from 'contentlayer/generated';
 
-export function BlogList() {
+export type BlogListProps = {
+  posts: Post[];
+};
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio';
 
-  const posts = query
-    ? allPosts.filter((post) =>
+  const postList = query
+    ? posts.filter((post) =>
       post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
     )
-    : allPosts;
+    : posts;
 
   const hasPosts = posts?.length > 0;
 
@@ -43,7 +48,7 @@ export function BlogList() {
       {/* Listagem de posts */}
       {hasPosts ? (
         <PostGridCard>
-          {posts.map((post) => (
+          {postList.map((post) => (
             <PostCard
               key={post._id}
               title={post.title}
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,19 @@
-import { BlogList } from "@/components/templates/blog";
+import { BlogList, type BlogListProps } from '@/components/templates/blog';
+import { allPosts } from 'contentlayer/generated';
+import { GetStaticProps } from 'next';
 
-export default function BlogPage() {
-  return <BlogList />;
+export default function BlogPage({ posts }: BlogListProps) {
+  return <BlogList posts={posts} />;
 }
+
+export const getStaticProps = (async () => {
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  return {
+    props: {
+      posts: sortedPosts,
+    },
+  };
+}) satisfies GetStaticProps<BlogListProps>;
